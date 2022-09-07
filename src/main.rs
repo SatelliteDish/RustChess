2cdc0ec7fca74d75dcff25acaec946bb0b5c8561
SatelliteDish <94733164+SatelliteDish@users.noreply.github.com>
2022-09-07 23:22:59 +0000

    Working on initializing board to start state

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,135 @@
+use colored::Colorize;
+
+const BOARD_SIZE: i32 = 8;
+
+#[derive(Clone)]
 enum Color{
     White,
     Black
 }
 
-struct Location{
-    x : i32,
-    y : i32,
+enum PieceType{
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
+}
+struct Piece{
+    color: Color,
+    position: Postition,
+    pieceType: PieceType,
 }
 
-enum Pieces{
-    Pawn{color: Color, loc: Location},
-    Bishop{color: Color, loc: Location},
-    Knight{color: Color, loc: Location},
-    Rook{color: Color, loc: Location},
-    Queen{color: Color, loc: Location},
-    King{color: Color, loc: Location}
+impl Piece {
+    fn move_piece(position: Postition){
+
+    }
 }
 
-const BOARD_SIZE: i8 = 8;
+struct Postition {
+    x: i32,
+    y: i32,
+}
+
+struct Cell {
+    color: Color,
+    piece: Option<Piece>,
+}
 
 fn main() {
+    let board = init_board();
+    print_board(&board);
+}
+
+fn init_board() -> Vec<Vec<Cell>> {
+    let mut board: Vec<Vec<Cell>> = Vec::new();
+
+    let mut next = Color::White;
+
     for row in 0..BOARD_SIZE {
         let mut s = String::new();
-        let mut next: Color = {
-            if(row%2 == 0){
-                Color::White
-            }
-            else{
-                Color::Black
-            }
-        };
+        if row%2 == 0 {
+            next = Color::White;
+        }
+        else{
+            next = Color::Black;
+        }
         
+        board.push(Vec::new());
+
         for col in 0..BOARD_SIZE {
+            let pos = build_position(row, col);
+            let piece: Option<Piece> = {
+                let col = {
+                    if row < BOARD_SIZE/2 {
+                        Color::White
+                    }
+                    else {
+                        Color::Black
+                    }
+                };
+                if row == 0 || row == BOARD_SIZE - 1 {
+                    None
+                }
+                else if row == 1 || row == BOARD_SIZE - 2 {
+                    Some(build_piece(col,pos,PieceType::Pawn))
+                }
+                else {
+                    None
+                }
+            };
+            board[row as usize].push(build_cell(next.clone(), None));
             match next {
-                Color::White =>{
-                    s = s + "⬜";
+                Color::White => {
                     next = Color::Black;
                 },
-                Color::Black =>{
-                    s = s + "⬛";
+                Color::Black => {
                     next = Color::White;
+                },
+            };
+        }
+    }
+
+    board
+}
+
+fn print_board(board: &Vec<Vec<Cell>>) {
+    for row in board.iter() {
+        let mut s = String::new();
+        for col in row.iter() {
+            match col.color {
+                Color::White => {
+                    s = s + "⬜";
+                },
+                Color::Black => {
+                    s = s + "⬛";
                 },
             }
         }
         println!("{}",s);
+    }
+}
+
+fn build_cell(color: Color, piece: Option<Piece>) -> Cell {
+    Cell{
+        color,
+        piece,
+    }
+}
+
+fn build_piece(color: Color, position: Postition, pieceType: PieceType) -> Piece {
+    Piece {
+        color,
+        position,
+        pieceType,
+    }
+}
+
+fn build_position(x: i32, y: i32) -> Postition {
+    Postition {
+        x,
+        y,
     }
 }
