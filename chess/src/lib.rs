7df3eca68cb62a85ad79c50bc8e5e72f678e63cb
SatelliteDish <94733164+SatelliteDish@users.noreply.github.com>
2022-10-07 23:43:27 +0000

    started to add pieces

--- chess/src/lib.rs
+++ chess/src/lib.rs
@@ -3,11 +3,11 @@ pub mod set {
     pub const BOARD_SIZE: usize = 8;
     #[derive(Debug)]
     pub struct Board {
-        pub board: Vec<Vec<Cell>>,
+        pub cells: Vec<Vec<Cell>>,
     }
     impl Board {
         pub fn new() -> Board {
-            let mut board: Vec<Vec<Cell>> = Vec::new();
+            let mut cells: Vec<Vec<Cell>> = Vec::new();
             for _row in 0..BOARD_SIZE {
                 let mut color = Color::Black;
                 let mut cell_row = Vec::new();
@@ -15,9 +15,9 @@ pub mod set {
                     cell_row.push(Cell{color});
                     color = !color;
                 }
-                board.push(cell_row);
+                cells.push(cell_row);
             }
-            Board { board }
+            Board { cells }
         }
     }
     #[derive(Debug)]
@@ -48,8 +48,8 @@ mod tests {
     fn board_is_valid_size() {
         let expected_size = BOARD_SIZE;
         let test_board = Board::new();
-        let length = test_board.board.len();
-        let height = test_board.board[0].len();
+        let length = test_board.cells.len();
+        let height = test_board.cells[0].len();
         assert!({
             let proper_ratio = { length == height };
             let proper_size = { length == expected_size };
@@ -62,15 +62,37 @@ mod tests {
         let mut is_valid = true;
         let mut is_white = false;
         let board = Board::new();
-        for _row in board.board.iter() {
+        for _row in board.cells.iter() {
             for _col in _row.iter() {
                 let is_black = _col.color == Color::Black;
                 if (is_white && is_black) || (!is_white && !is_black) {
                     is_valid = false;
+                    break;
                 }
                 is_white = !is_white;
             }
         }
         assert!(is_valid);
     }
+
+    #[test]
+    fn start_position_is_correct() {
+        let board = Board::Start();
+        let mut is_valid = true;
+        for row in board.cells.iter() {
+            for col in row.iter() {
+                let piece: Option<Piece> = col.get_piece();
+                if (row == 0 || BOARD_SIZE - 1) || (row == 1 || BOARD_SIZE - 2) {
+                    let mut _type: PieceType;
+                    match piece {
+                        Some(t) => _type = t,
+                        None => {
+                            is_valid = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    } 
 }
